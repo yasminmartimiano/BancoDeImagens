31aa684514278d6aa661072a47543a622a33eef0
yasminmartimiano <yasmin52@gmail.com>
2025-06-04 21:35:08 -0300

    first commit

---
+++ src/main/java/com/yasminmartimiano/BancoDeImagens/BancoDeImagensApplication.java
@@ -0,0 +1,13 @@
+package com.yasminmartimiano.BancoDeImagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoDeImagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoDeImagensApplication.class, args);
+	}
+
+}
