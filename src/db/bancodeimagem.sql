ddf3fe436a6a9e3a62fd42c3be0aac19a65ad9aa
yasminmartimiano <yasminliver52@gmail.com>
2025-06-05 14:06:56 -0300

    feat: implementou Imagem Service

--- src/db/bancodeimagem.sql
+++ src/db/bancodeimagem.sql
@@ -2,3 +2,8 @@ CREATE DATABASE bancodeimagem;
 
 USE bancodeimagem;
 
+CREATE Table imagem (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    url TEXT NOT NULL
+);
--- src/main/java/com/yasminmartimiano/BancoDeImagens/Model/ImagemModel.java
+++ src/main/java/com/yasminmartimiano/BancoDeImagens/Model/ImagemModel.java
@@ -1,5 +1,25 @@
 package com.yasminmartimiano.BancoDeImagens.Model;
 
-public class ImagemModel {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name="imagens")
+@NoArgsConstructor //criando construtor vazio
+@AllArgsConstructor // construtor cheio
+@Data //criando getters e setters
+public class ImagemModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @Column(columnDefinition = "TEXT")
+    private String url;
 }
