c912c8beca68bf49a9f3d1f0188c81f6ada84dd2
yasminmartimiano <yasminliver52@gmail.com>
2025-06-08 05:15:06 -0300

    feat: adicionando um post que recebe a imagem no bd

--- src/main/java/com/yasminmartimiano/BancoDeImagens/Controller/ImagemController.java
+++ src/main/java/com/yasminmartimiano/BancoDeImagens/Controller/ImagemController.java
@@ -1,7 +1,9 @@
 package com.yasminmartimiano.BancoDeImagens.Controller;
 
+import java.io.IOException;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.yasminmartimiano.BancoDeImagens.Model.ImagemModel;
 import com.yasminmartimiano.BancoDeImagens.Service.ImagemService;
@@ -24,6 +28,20 @@ public class ImagemController {
     public ImagemController(ImagemService imagemService){
         this.imagemService = imagemService;
     }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
+    try {
+        
+        byte[] imageData = file.getBytes();
+
+        return ResponseEntity.ok("Imagem recebida com sucesso!");
+        
+    } catch (IOException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao processar imagem.");
+    }
+    }
+
 
     @GetMapping
     public List<ImagemModel> listarImagens(){
